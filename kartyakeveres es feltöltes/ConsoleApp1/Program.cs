ddeb4d9184b65ff16598af2779ba86e6307e7610
zumi9i <103568791+zumi9i@users.noreply.github.com>
2024-02-19 13:08:37 +0100

    Add files via upload

---
+++ kartyakeveres es feltöltes/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] uniqueNumbers = GenerateUniqueRandomNumbers(1, 9, 9); 
+        int[] numbersTwice = uniqueNumbers.Concat(uniqueNumbers).ToArray();
+        ShuffleArray(numbersTwice);
+
+        
+        Console.WriteLine("Array with each number appearing twice:");
+        foreach (var number in numbersTwice)
+        {
+            Console.Write(number + " ");
+        }
+        
+        Console.ReadKey();
+    }
+
+    static int[] GenerateUniqueRandomNumbers(int min, int max, int count)
+    {
+        Random random = new Random();
+        return Enumerable.Range(min, max - min + 1)
+                         .OrderBy(x => random.Next())
+                         .Take(count)
+                         .ToArray();
+    }
+
+    static void ShuffleArray(int[] array)
+    {
+        Random random = new Random();
+        int n = array.Length;
+        for (int i = 0; i < n; i++)
+        {
+          
+            int randomIndex = i + random.Next(n - i);
+            int temp = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
+    }
+}
